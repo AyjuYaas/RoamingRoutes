39875dd4f175a25def906e11c6f1a4281288abe1
SAYUJYA SATYAL <sayujya57@gmail.com>
2024-05-11 20:27:50 +0545

    Fix navbar scroll and aboutme section

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,4 +1,4 @@
-import { useRef } from "react";
+import { useState, useEffect, useRef } from "react";
 import { FaBars } from "react-icons/fa";
 
 import Logo from "./items/Logo";
@@ -7,9 +7,25 @@ import SocialLinks from "./items/SocialLinks";
 import "./navstyles.css";
 
 const NavBar = () => {
+  let a = 0;
   const navRef = useRef();
+  const [isBodyOverflowHidden, setIsBodyOverflowHidden] = useState(false);
+
+  useEffect(() => {
+    if (isBodyOverflowHidden) {
+      document.body.style.overflow = "hidden";
+    } else {
+      document.body.style.overflow = "visible";
+    }
+
+    return () => {
+      document.body.style.overflow = "visible"; // Reset overflow when component unmounts
+    };
+  }, [isBodyOverflowHidden]);
+
   const showNavbar = () => {
     navRef.current.classList.toggle("responsive-nav");
+    setIsBodyOverflowHidden((prevState) => !prevState);
   };
   return (
     <div className="navbar">
